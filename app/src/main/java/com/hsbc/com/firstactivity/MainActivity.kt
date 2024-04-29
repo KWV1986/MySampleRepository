50c78c9a5933431b18e2b900dd4934132bff8bb9
deepak <deepak.borade@gmail.com>
2024-04-29 12:55:51 +0530

    create a function for reverse the string

--- app/src/main/java/com/hsbc/com/firstactivity/MainActivity.kt
+++ app/src/main/java/com/hsbc/com/firstactivity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hsbc.com.firstactivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,10 +11,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val category = Categoty(10,"manufacturing")
-
+        val result= reverseString("deepak borade")
+        Log.i("Main", result)
         val intent = Intent(this@MainActivity,SecondActivity::class.java)
         intent.putExtra("category",category)
         startActivity(intent)
 
     }
+    private fun reverseString(name: String): String {
+        var word = ""
+        var result = ""
+        for (char in name) {
+            if (char != ' ') {
+                word = char + word
+            } else {
+                result += word + ""
+                word = ""
+            }
+        }
+        result += word
+        return result
+    }
+
 }
